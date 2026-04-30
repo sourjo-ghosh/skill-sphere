82f277ca752d88823c20807c664d94d7ccb4ab20
sourjo-ghosh <joyghosh211@outlook.com>
2026-05-01 01:10:18 +0600

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/shared/Navbar";
+import { ThemeProvider } from "next-themes";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,9 +22,14 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased suppressHydrationWarning`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <Navbar />
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
---
+++ src/components/ThemeToggle.jsx
@@ -0,0 +1,13 @@
+import { useTheme } from "next-themes";
+
+
+const ThemeToggle = () => {
+    const {theme, setTheme} = useTheme()
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ThemeToggle;
