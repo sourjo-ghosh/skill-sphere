bb1e7246669a46675ce1edaca684b5ead12126d4
sourjo-ghosh <joyghosh211@outlook.com>
2026-05-03 06:24:14 +0600

    fix: update fetch URLs to use better authentication environment variable

--- src/app/courses/page.jsx
+++ src/app/courses/page.jsx
@@ -12,7 +12,7 @@ const allCoursersPage = () => {
   useEffect(() => {
     const fetchCourses = async () => {
       try {
-        const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/data.json`);;
+        const res = await fetch(`${process.env.NEXT_PUBLIC_BETTER_AUTH_URL}/data.json`);;
         if (!res.ok) {
           throw new Error(`Failed to load courses: ${res.status}`);
         }
